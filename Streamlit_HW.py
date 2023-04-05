1d64fdd41ae5c2829ac1eb55c8700086d0485c33
Kelly Blumhagen <kellyblumhagen@gmail.com>
2023-04-05 17:25:33 -0400

    Update Streamlit_HW.py

--- Streamlit_HW.py
+++ Streamlit_HW.py
@@ -1,73 +1,49 @@
 import streamlit as st
-import random
-import altair as alt
-import numpy as np
 import pandas as pd
-st.header('Homework 1')
-st.markdown(
-"**QUESTION 1**: In previous homeworks you created dataframes from random numbers.\n"
-"Create a datframe where the x axis limit is 100 and the y values are random values.\n"
-"Print the dataframe you create and use the following code block to help get you started"
-)
-st.code(
-'''
-x_limit =
-# List of values from 0 to 100 each value being 1 greater than the last
-x_axis = np.arange()
-# Create a random array of data that we will use for our y values
-y_data = []
-df = pd.DataFrame({'x': x_axis,
-                     'y': y_data})
-st.write(df)''',language='python')
-x_limit=100
-x_axis=np.arange(start=0, stop=x_limit, step=1)
-y_data = [random.random() for value in x_axis]
-df = pd.DataFrame({'x': x_axis,
-                     'y': y_data})
-st.write(df)
-st.markdown(
-"**QUESTION 2**: Using the dataframe you just created, create a basic scatterplot and Print it.\n"
-"Use the following code block to help get you started."
-)
-st.code(
-'''
-scatter = alt.Chart().mark_point().encode()
-st.altair_chart(scatter, use_container_width=True)''',language='python')
-scatter = alt.Chart(data=df).mark_point().encode(x='x',y='y')
-st.altair_chart(scatter, use_container_width=True)
-st.markdown(
-"**QUESTION 3**: Lets make some edits to the chart by reading the documentation on Altair.\n"
-"https://docs.streamlit.io/library/api-reference/charts/st.altair_chart.  "
-"Make 5 changes to the graph, document the 5 changes you made using st.markdown(), and print the new scatterplot.  \n"
-"To make the bullet points and learn more about st.markdown() refer to the following discussion.\n"
-"https://discuss.streamlit.io/t/how-to-indent-bullet-point-list-items/28594/3"
-)
-scatter1 = alt.Chart(data=df).mark_point(size=60,opacity=0.5, color='red').encode(x='x',y='y',
-    tooltip=['x', 'y'])
-scatter1.configure_header(
-    titleColor='green',
-    titleFontSize=14,
-    labelColor='red',
-    labelFontSize=14
-)
-st.altair_chart(scatter1, use_container_width=True)
-st.markdown("The five changes I made were.....")
-st.markdown("""
-The 5 changes I made were:
-- Changed the size of the point
-- Added a tool tip
-- made the color red
-- changed the opacity to 50%
-- title
-""")
-st.markdown(
-"**QUESTION 4**: Explore on your own!  Go visit https://altair-viz.github.io/gallery/index.html.\n "
-"Pick a random visual, make two visual changes to it, document those changes, and plot the visual.  \n"
-"You may need to pip install in our terminal for example pip install vega_datasets "
-)
-st.markdown("""
-The 2 changes I made were:
-- Change 1
-- Change 2
-"""
-)
+import numpy as np
+import altair as alt
+from datetime import time, datetime
+import plotly.express as px
+
+# Post 2 links. One to your GitHub repository and one to your published Streamlit app.  
+# This is an interactive application that you create preferably using your midterm data, but if that is not possible use any data of your choice.  
+# Create at least one visual that has two interactions that the user can change.
+
+df = pd.read_csv("ODs.csv")
+
+st.set_page_config(layout = "wide")
+st.header("Overdose Deaths")
+
+page = st.sidebar.selectbox("Select Scope",
+  ["State", "Regional"])
+
+if page == "State":
+  
+## States
+  state_list = df["State"].unique()
+  state = st.selectbox("Select a State:",state_list)
+  col1, col2 = st.columns(2)
+  fig = px.line(df[df["State"] == state], 
+    x = "Year", y = "Opioid_Overdose_Death_Rate",title = "Opioid Overdose Deaths")
+ 
+  col1.plotly_chart(fig,use_container_width = True)
+  fig = px.line(df[df["State"] == state], 
+    x = "Year", y = "All_Drug_Overdose_Death_Rate",title = "All Drug Overdose Deaths")
+  
+  col2.plotly_chart(fig,use_container_width = True)
+else:
+  
+## Regions
+  region_list = df["Region"].unique()
+ 
+  region = st.selectbox("Select a Region:",region_list)
+  col1,col2 = st.columns(2)
+  fig = px.line(df[df['Region'] == region], 
+    x = "Year", y = "Opioid_Overdose_Death_Rate",
+    title = "Opioid Overdose Deaths",color = "Region")
+  
+  col1.plotly_chart(fig)
+  fig = px.line(df[df["Region"] == region], x = "Year", y = "All_Drug_Overdose_Death_Rate",
+  title = "All Drug Overdose Deaths",color = "Region")
+  
+  col2.plotly_chart(fig, use_container_width = True)
